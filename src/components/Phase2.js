8854e78a27227323d185dfcb22e919d69a222920
David <dada7117@gmail.com>
2021-05-31 15:34:29 +0300

    test

--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -1,27 +1,68 @@
-import React,{useState} from 'react'
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 
+function Phase2({ onLogin, errorMsg }) {
+  const [cityInput, setCityInput] = useState("");
+  const [addressInput, setAddressInput] = useState("");
+  const [phoneInput, setPhoneInput] = useState("");
 
-function Phase2() {
-    return (
-        <div>
-            <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>City</Form.Label>
-    <Form.Control type="text" placeholder="Enter City" />
-  </Form.Group>
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onLogin(loginData.email.value, loginData.password.value);
+  };
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
- 
-  <Button variant="primary">
-    Submit
-  </Button>
-</Form>
-        </div>
-    )
+  const [loginData, setLoginData] = useState({
+    email: {
+      value: '', 
+      errors:[], 
+      validations:{
+        required: true, 
+        pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ 
+      }
+    },
+    password: {
+      value: '', 
+      errors: [], 
+      validations: {
+        required: true, 
+        pattern: /^[0-9a-zA-Z]{6}$/g
+      }
+    }
+  })
+
+  return (
+    <Container>
+      <Form onSubmit={onSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            onChange={(e) => setCityInput(e.target.value)}
+            type="text"
+            placeholder="Enter City"
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            onChange={(e) => setAddressInput(e.target.value)}
+            type="text"
+            placeholder="Enter Address"
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control
+            onChange={(e) => setPhoneInput(e.target.value)}
+            type="phone"
+            placeholder="Password"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type='submit'>Submit</Button>
+      </Form>
+    </Container>
+  );
 }
 
-export default Phase2
+export default Phase2;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/Phase2';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
