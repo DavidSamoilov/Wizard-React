b563fb735f7155005aa9e74d621e81339791226e
David <dada7117@gmail.com>
2021-05-31 16:31:05 +0300

    1

--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -1,48 +1,31 @@
-import React,{useState} from 'react'
-import { Form, Button,Container } from "react-bootstrap";
-
+import React, { useState } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 
 function Phase2() {
-   
   return (
-    <Container>
+    <div>
       <Form>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="Enter City" />
+          <Form.Control type="text" placeholder="Enter City" name="city" />
         </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Address</Form.Label>
-          <Form.Control type="text" placeholder="Enter Address" />
-        </Form.Group>
-          <Form.Control type='text' placeholder='Enter City' name='city' />
-        </Form.Group>
-
-        <Form.Group controlId='formBasicPassword'>
-          <Form.Label>Street</Form.Label>
-          <Form.Control type='text' placeholder='Enter Street' name='street' />
-        </Form.Group>
-        <Form.Group controlId='formBasicPassword'>
-          <Form.Label>Number</Form.Label>
-          <Form.Control
-            type='number'
-            placeholder='Enter Number'
-            name='number'
-          />
-        </Form.Group>
-        <Button variant='primary mr-2'>Back</Button>
-        <Button variant='primary ml-2'>Submit</Button>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="phone" placeholder="Password" />
+          <Form.Label>Street</Form.Label>
+          <Form.Control type="text" placeholder="Enter Street" name="street" />
         </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Number</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Enter Number"
+            name="number"
+          />
+        </Form.Group>
+        <Button variant="primary mr-2">Back</Button>
+        <Button variant="primary ml-2">Submit</Button>
       </Form>
-    </Container>
+    </div>
   );
 }
 
