39daad129c5eccc0546ed06b787ac3a02183253c
David <dada7117@gmail.com>
2021-05-31 15:47:43 +0300

    phase 2 changes

--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -1,27 +1,32 @@
 import React,{useState} from 'react'
-import { Form, Button } from "react-bootstrap";
+import { Form, Button,Container } from "react-bootstrap";
 
 
 function Phase2() {
-    return (
-        <div>
-            <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>City</Form.Label>
-    <Form.Control type="text" placeholder="Enter City" />
-  </Form.Group>
+   
+  return (
+    <Container>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="Enter City" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type="text" placeholder="Enter Address" />
+        </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
- 
-  <Button variant="primary">
-    Submit
-  </Button>
-</Form>
-        </div>
-    )
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="phone" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
 export default Phase2
