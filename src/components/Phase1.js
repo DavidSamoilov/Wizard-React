fe1e8e5fe91d07e779510e6afb2677fe94187e76
leebaronx3 <leebaronx3@gmail.com>
2021-05-31 16:44:54 +0300

    added route to phase2

--- src/components/Phase1.js
+++ src/components/Phase1.js
@@ -1,27 +1,34 @@
 import { Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import Phase2 from './Phase2'
 
 function Phase1() {
-  return (
-    <Form>
-      <Form.Group className='mb-3' controlId='formName'>
-        <Form.Label>Name</Form.Label>
-        <Form.Control type='text' name='name' placeholder='Enter Name' />
-      </Form.Group>
 
-      <Form.Group className='mb-3' controlId='formEmail'>
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type='email' placeholder='Enter email' name='email' />
-      </Form.Group>
+    const history = useHistory();
+    const handleClick = () => history.push('/Phase2');
 
-      <Form.Group controlId='formFile' className='mb-3'>
-        <Form.Label>Date of Birth:</Form.Label>
-        <Form.Control type='date' name='dateOfBirth' />
-      </Form.Group>
 
-      <Button variant='primary' type='submit'>
-        Next
+    return (
+        <Form>
+            <Form.Group className='mb-3' controlId='formName'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control type='text' name='name' placeholder='Enter Name' />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formEmail'>
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type='email' placeholder='Enter email' name='email' />
+            </Form.Group>
+
+            <Form.Group controlId='formFile' className='mb-3'>
+                <Form.Label>Date of Birth:</Form.Label>
+                <Form.Control type='date' name='dateOfBirth' />
+            </Form.Group>
+
+            <Button variant='primary' onClick={handleClick}>
+                Next
       </Button>
-    </Form>
-  );
+        </Form>
+    );
 }
 export default Phase1;
