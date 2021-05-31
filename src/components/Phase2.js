ec26cf319b0a256ac8ebc10ec6984b65061b8064
David <dada7117@gmail.com>
2021-05-31 15:46:49 +0300

    test

--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -2,30 +2,7 @@ import React, { useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
 
 function Phase2() {
-  //   const onSubmit = (e) => {
-  //     e.preventDefault();
-  //     onLogin(loginData.email.value, loginData.password.value);
-  //   };
-
-  const [loginData, setLoginData] = useState({
-    email: {
-      value: "",
-      errors: [],
-      validations: {
-        required: true,
-        pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      },
-    },
-    password: {
-      value: "",
-      errors: [],
-      validations: {
-        required: true,
-        pattern: /^[0-9a-zA-Z]{6}$/g,
-      },
-    },
-  });
-
+  
   return (
     <Container>
       <Form>
