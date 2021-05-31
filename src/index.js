9a41bb46b55fd217816575d81a36ee3e33a502f0
NadavLalo <nadavlalo@gmail.com>
2021-05-31 14:43:56 +0300

    Added bootstrap import and bootstrap links

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/Phase2';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
