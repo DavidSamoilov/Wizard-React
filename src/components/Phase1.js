9235cacfaab1fffda9a3e3894337ade60a51413f
NadavLalo <nadavlalo@gmail.com>
2021-05-31 16:56:26 +0300

    format

--- src/components/Phase1.js
+++ src/components/Phase1.js
@@ -1,34 +1,32 @@
 import { Form, Button } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
-import Phase2 from './Phase2'
+import Phase2 from './Phase2';
 
 function Phase1() {
+  const history = useHistory();
+  const handleClick = () => history.push('/Phase2');
 
-    const history = useHistory();
-    const handleClick = () => history.push('/Phase2');
+  return (
+    <Form>
+      <Form.Group className='mb-3' controlId='formName'>
+        <Form.Label>Name</Form.Label>
+        <Form.Control type='text' name='name' placeholder='Enter Name' />
+      </Form.Group>
 
+      <Form.Group className='mb-3' controlId='formEmail'>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type='email' placeholder='Enter email' name='email' />
+      </Form.Group>
 
-    return (
-        <Form>
-            <Form.Group className='mb-3' controlId='formName'>
-                <Form.Label>Name</Form.Label>
-                <Form.Control type='text' name='name' placeholder='Enter Name' />
-            </Form.Group>
+      <Form.Group controlId='formFile' className='mb-3'>
+        <Form.Label>Date of Birth:</Form.Label>
+        <Form.Control type='date' name='dateOfBirth' />
+      </Form.Group>
 
-            <Form.Group className='mb-3' controlId='formEmail'>
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type='email' placeholder='Enter email' name='email' />
-            </Form.Group>
-
-            <Form.Group controlId='formFile' className='mb-3'>
-                <Form.Label>Date of Birth:</Form.Label>
-                <Form.Control type='date' name='dateOfBirth' />
-            </Form.Group>
-
-            <Button variant='primary' onClick={handleClick}>
-                Next
+      <Button variant='primary' onClick={handleClick}>
+        Next
       </Button>
-        </Form>
-    );
+    </Form>
+  );
 }
 export default Phase1;
--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 function Phase2() {
   const history = useHistory();
-  const submitToPhase3 = () => history.push("/phase3");
-  const goBackToPhase1 = () => history.push("/phase1");
+  const submitToPhase3 = () => history.push('/phase3');
+  const goBackToPhase1 = () => history.push('/phase1');
 
   return (
     <Form>
-      <Form.Group controlId="city">
+      <Form.Group controlId='city'>
         <Form.Label>City</Form.Label>
-        <Form.Control type="text" placeholder="Enter City" name="city" />
+        <Form.Control type='text' placeholder='Enter City' name='city' />
       </Form.Group>
 
-      <Form.Group controlId="street">
+      <Form.Group controlId='street'>
         <Form.Label>Street</Form.Label>
-        <Form.Control type="text" placeholder="Enter Street" name="street" />
+        <Form.Control type='text' placeholder='Enter Street' name='street' />
       </Form.Group>
-      <Form.Group controlId="number">
+      <Form.Group controlId='number'>
         <Form.Label>Number</Form.Label>
-        <Form.Control type="number" placeholder="Enter Number" name="number" />
+        <Form.Control type='number' placeholder='Enter Number' name='number' />
       </Form.Group>
-      <Button variant="primary mr-2" onClick={goBackToPhase1}>
+      <Button variant='primary mr-2' onClick={goBackToPhase1}>
         Back
       </Button>
-      <Button variant="primary ml-2" onClick={submitToPhase3}>
+      <Button variant='primary ml-2' onClick={submitToPhase3}>
         Submit
       </Button>
     </Form>
