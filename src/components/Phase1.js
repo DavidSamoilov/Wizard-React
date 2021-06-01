be8f70ac09ec82ba825d541ad6ece90036c61a92
leebaronx3 <leebaronx3@gmail.com>
2021-06-01 10:35:13 +0300

    added error componenet and adjusted phase1

--- src/components/Phase1.js
+++ src/components/Phase1.js
@@ -1,26 +1,7 @@
 import { Form, Button } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import { useState } from 'react';
-
-// {
-//   image: {
-//     value: '',
-//       errors: [],
-//         validations: {
-//       required: true,
-//         pattern:
-//       '/^(?:(?:https?|ftp)://)(?:S+(?::S*)?@)?(?:(?!(?:10|127)(?:.d{1,3}){3})(?!(?:169.254|192.168)(?:.d{1,3}){2})(?!172.(?:1[6-9]|2d|3[0-1])(?:.d{1,3}){2})(?:[1-9]d?|1dd|2[01]d|22[0-3])(?:.(?:1?d{1,2}|2[0-4]d|25[0-5])){2}(?:.(?:[1-9]d?|1dd|2[0-4]d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:.(?:[a-z\u00a1-\uffff]{2,})).?)(?::d{2,5})?(?:[/?#]S*)?$/i',
-//       },
-//   },
-
-//   hobbies: {
-//     value: '',
-//       errors: [],
-//         validations: {
-//       required: false,
-//       },
-//   },
-// }
+import ErrorMessages from './ErrorMessages';
 
 function Phase1({ onChange, onValidate }) {
   const [phase1Data, setPhaseData] = useState({
@@ -37,15 +18,15 @@ function Phase1({ onChange, onValidate }) {
       value: '',
       errors: [],
       validations: {
-        required: false,
+        required: true,
         pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
       },
     },
-    dateOfBirth: {
+    dob: {
       value: '',
       errors: [],
       validations: {
-        required: false,
+        required: true,
       },
     },
   })
@@ -71,6 +52,7 @@ function Phase1({ onChange, onValidate }) {
         <Form.Control type='text' name='name' placeholder='Enter Name'
           onChange={(e) => onChange(e, phase1Data, setPhaseData)}
           onBlur={(e) => onValidate(e, phase1Data, setPhaseData)} />
+        <ErrorMessages errors={phase1Data.name.errors} />
       </Form.Group>
 
       <Form.Group className='mb-3' controlId='formEmail'>
@@ -78,13 +60,15 @@ function Phase1({ onChange, onValidate }) {
         <Form.Control type='email' placeholder='Enter email' name='email'
           onChange={(e) => onChange(e, phase1Data, setPhaseData)}
           onBlur={(e) => onValidate(e, phase1Data, setPhaseData)} />
+        <ErrorMessages errors={phase1Data.email.errors} />
       </Form.Group>
 
       <Form.Group controlId='formFile' className='mb-3'>
         <Form.Label>Date of Birth:</Form.Label>
-        <Form.Control type='date' name='dateOfBirth'
+        <Form.Control type='date' name='dob'
           onChange={(e) => onChange(e, phase1Data, setPhaseData)}
           onBlur={(e) => onValidate(e, phase1Data, setPhaseData)} />
+        <ErrorMessages errors={phase1Data.dob.errors} />
       </Form.Group>
 
       <Button variant='primary' onClick={handleClick}>
