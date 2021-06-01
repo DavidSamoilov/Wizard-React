d49197ef4d2de6b2f8b9544e40853e91502901ad
David <dada7117@gmail.com>
2021-06-01 12:34:14 +0300

    t

--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -67,9 +67,27 @@ function Phase2({ onChange, onValidate }) {
   const submitToPhase3 = () => {
     // replace with validation
     if (true) {
+      let error = false
       localStorage.setItem("phase2Data", JSON.stringify(phase2Data));
-
-      history.push("/phase3");
+      for (let input in phase2Data) {
+        console.log(phase2Data[input]);
+        if (!phase2Data[input].validations.pattern.test(phase2Data[input].value)) {
+          setPhase2Data({
+            ...phase2Data,
+            [input]: {
+              ...phase2Data[input],
+              errors:[`${input} is invalid`],
+            },
+          })
+          error = true
+          
+        }
+      }
+      if (error){
+        return 0;
+      }else{
+        history.push("/phase3");
+      }
     }
   };
 
