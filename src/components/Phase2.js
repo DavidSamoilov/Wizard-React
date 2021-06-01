0751fb6efc991a300d932d0d37052ce470cf462e
David <dada7117@gmail.com>
2021-06-01 10:04:26 +0300

    t

--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -6,9 +6,33 @@ function Phase2() {
   const history = useHistory();
   const goBackToPhase1 = () => history.push("/");
 
-  const [cityInput, setCityInput] = useState("");
-  const [streetInput, setStreetInput] = useState("");
-  const [numberInput, setNumberInput] = useState("");
+  const [cityInput, setCityInput] = useState({city: {
+    value: '',
+    errors: [],
+    validations: {
+      required: true,
+      pattern:
+        '/^(?:(?:https?|ftp)://)(?:S+(?::S*)?@)?(?:(?!(?:10|127)(?:.d{1,3}){3})(?!(?:169.254|192.168)(?:.d{1,3}){2})(?!172.(?:1[6-9]|2d|3[0-1])(?:.d{1,3}){2})(?:[1-9]d?|1dd|2[01]d|22[0-3])(?:.(?:1?d{1,2}|2[0-4]d|25[0-5])){2}(?:.(?:[1-9]d?|1dd|2[0-4]d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:.(?:[a-z\u00a1-\uffff]{2,})).?)(?::d{2,5})?(?:[/?#]S*)?$/i',
+    },
+  }});
+  const [streetInput, setStreetInput] = useState({street: {
+    value: '',
+    errors: [],
+    validations: {
+      required: true,
+      pattern:
+        '/^(?:(?:https?|ftp)://)(?:S+(?::S*)?@)?(?:(?!(?:10|127)(?:.d{1,3}){3})(?!(?:169.254|192.168)(?:.d{1,3}){2})(?!172.(?:1[6-9]|2d|3[0-1])(?:.d{1,3}){2})(?:[1-9]d?|1dd|2[01]d|22[0-3])(?:.(?:1?d{1,2}|2[0-4]d|25[0-5])){2}(?:.(?:[1-9]d?|1dd|2[0-4]d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:.(?:[a-z\u00a1-\uffff]{2,})).?)(?::d{2,5})?(?:[/?#]S*)?$/i',
+    },
+  }});
+  const [numberInput, setNumberInput] = useState({number: {
+    value: '',
+    errors: [],
+    validations: {
+      required: true,
+      pattern:
+        '/^(?:(?:https?|ftp)://)(?:S+(?::S*)?@)?(?:(?!(?:10|127)(?:.d{1,3}){3})(?!(?:169.254|192.168)(?:.d{1,3}){2})(?!172.(?:1[6-9]|2d|3[0-1])(?:.d{1,3}){2})(?:[1-9]d?|1dd|2[01]d|22[0-3])(?:.(?:1?d{1,2}|2[0-4]d|25[0-5])){2}(?:.(?:[1-9]d?|1dd|2[0-4]d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:.(?:[a-z\u00a1-\uffff]{2,})).?)(?::d{2,5})?(?:[/?#]S*)?$/i',
+    },
+  }});
 
   const submitToPhase3 = () => {
     // replace with validation
@@ -29,7 +53,7 @@ function Phase2() {
       <Form.Group controlId="city">
         <Form.Label>City</Form.Label>
         <Form.Control
-          onChange={(e) => setCityInput(e.target.value)}
+          onChange={(e) => setCityInput()}
           type="text"
           placeholder="Enter City"
           name="city"
