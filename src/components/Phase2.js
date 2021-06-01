6903fdb11eb3e49608ca7a470bc24611cf7acbbe
David <dada7117@gmail.com>
2021-06-01 10:43:07 +0300

    t

--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -2,21 +2,73 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 
-function Phase2() {
+function Phase2({ onChange, onValidate }) {
   const history = useHistory();
   const goBackToPhase1 = () => history.push("/");
 
-  const [cityInput, setCityInput] = useState("");
-  const [streetInput, setStreetInput] = useState("");
-  const [numberInput, setNumberInput] = useState("");
+  const [cityInput, setCityInput] = useState({
+    city: {
+      value: "",
+      errors: [],
+      validations: {
+        required: true,
+        pattern: "/^.{1,35}$/",
+      },
+    },
+  });
+  const [streetInput, setStreetInput] = useState({
+    street: {
+      value: "",
+      errors: [],
+      validations: {
+        required: true,
+        pattern: "/^.{1,35}$/",
+      },
+    },
+  });
+  const [numberInput, setNumberInput] = useState({
+    number: {
+      value: "",
+      errors: [],
+      validations: {
+        required: true,
+        pattern: "^[1-9][0-9]*$",
+      },
+    },
+  });
 
+  const [phase2Data,setPhase2Data] =useState( {
+    
+      street: {
+        value: "",
+        errors: [],
+        validations: {
+          required: true,
+          pattern: "/^.{1,35}$/"
+        }
+      },
+        city: {
+          value: "",
+          errors: [],
+          validations: {
+            required: true,
+            pattern: "/^.{1,35}$/"
+          }
+        },
+      number: {
+        value: "",
+        errors: [],
+        validations: {
+          required: true,
+          pattern: "^[1-9][0-9]*$"
+        }
+      }}
+  )
+
+
+  
   const submitToPhase3 = () => {
     // replace with validation
-    const phase2Data = {
-      cityInput: cityInput,
-      streetInput: streetInput,
-      numberInput: numberInput,
-    };
     if (true) {
       localStorage.setItem("phase2Data", JSON.stringify(phase2Data));
 
@@ -29,7 +81,7 @@ function Phase2() {
       <Form.Group controlId="city">
         <Form.Label>City</Form.Label>
         <Form.Control
-          onChange={(e) => setCityInput(e.target.value)}
+          onChange={(e) => onChange(e, phase2Data, setPhase2Data)}
           type="text"
           placeholder="Enter City"
           name="city"
@@ -39,7 +91,7 @@ function Phase2() {
       <Form.Group controlId="street">
         <Form.Label>Street</Form.Label>
         <Form.Control
-          onChange={(e) => setStreetInput(e.target.value)}
+          onChange={(e) => onChange(e, phase2Data, setPhase2Data)}
           type="text"
           placeholder="Enter Street"
           name="street"
@@ -48,7 +100,7 @@ function Phase2() {
       <Form.Group controlId="number">
         <Form.Label>Number</Form.Label>
         <Form.Control
-          onChange={(e) => setNumberInput(e.target.value)}
+          onChange={(e) => onChange(e, phase2Data, setPhase2Data)}
           type="number"
           placeholder="Enter Number"
           name="number"
