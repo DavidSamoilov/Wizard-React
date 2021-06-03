d9256ab5fda58e932f838d9a367a62a7dd0d50ca
David <dada7117@gmail.com>
2021-06-03 07:38:46 +0300

    added reset button

---
+++ src/components/ResetDataButton.js
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button'
+
+const ResetDataButton = () => {
+    return (
+        <div>
+            <Button onClick={()=>onClick()}>
+                Reset Form</Button>
+        </div>
+    )
+}
+
+export default ResetDataButton
